34568a43fe4caa8ba64deb1c5d7e2741d406f2cb
DEAIFISH <1277676309@qq.com>
2023-07-16 16:33:34 +0800

    2023/7/16

---
+++ spring6-junit/src/main/java/com/DEAiFISH/junit5/SpringTestJunit5.java
@@ -0,0 +1,28 @@
+package com.DEAiFISH.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+//法一（常用）：
+@SpringJUnitConfig(locations = "classpath:bean.xml")
+
+//法二（不常用）：
+/*@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:bean.xml")*/
+public class SpringTestJunit5 {
+    //注入
+    @Autowired
+    private User user;
+
+    //测试方法
+    @Test
+    public void testUSer(){
+        System.out.println(user);
+        user.run();
+    }
+}
---
+++ spring6-junit/src/main/java/com/DEAiFISH/junit5/User.java
@@ -0,0 +1,10 @@
+package com.DEAiFISH.junit5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    public void run(){
+        System.out.println("user...");
+    }
+}
